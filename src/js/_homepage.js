c9ada41e550c1b6f3f6e8bcdb41dce299016aa8e
Tulika Das <tulikadas1029@gmail.com>
2022-04-21 17:56:31 +0530

    membership and mission statement section complete

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -5,12 +5,13 @@ import { TextPlugin } from "gsap/TextPlugin";
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
 window.addEventListener('load',() =>{
+    // about us section
     const headingText = document.querySelector('.about-us-heading').textContent
     gsap.set(".about-us-heading", {text:{value:''}})
 
     gsap.to(".about-us-heading", {
         scrollTrigger:{
-            trigger: ".about-us-heading",
+            trigger: ".about-us-section",
         },
         duration: 5,
         // repeat:-1,
@@ -36,4 +37,43 @@ window.addEventListener('load',() =>{
         ease: "power4.out",
         y:50
     })
+})
+
+window.addEventListener('load',() =>{
+    // gsap.from(".pointer", {
+    //     scrollTrigger:{
+    //         trigger: ".pointer",
+    //     },
+    //     duration: 3,
+    //     ease: "power4.out",
+    //     y:50
+    // })
+    const textAnimationTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".membership-benefits-section",
+          start: "center center",
+        }})
+
+    textAnimationTl.from(".membership-image", {
+        autoAlpha:0,
+        x:200,
+        ease: "power4.out",
+        duration: 0.9,
+    })
+
+    textAnimationTl.from(".register-text", {
+        y:200,
+        autoAlpha:0,
+        duration: 1,
+        ease: "Power2.easeOut",
+    })
+    
+    textAnimationTl.from(".pointer", {
+        autoAlpha:0,
+        y:50,
+        duration: 1,
+        ease: "power4.out",
+        stagger:0.3
+    })
+
 })
