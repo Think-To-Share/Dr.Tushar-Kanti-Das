bcf454abedc9b19b1cf84c60503df7ace2b03e60
Tulika Das <tulikadas1029@gmail.com>
2022-04-22 19:22:19 +0530

    blog section add and gsap section add

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -14,7 +14,6 @@ window.addEventListener('load',() =>{
             trigger: ".about-us-section",
         },
         duration: 5,
-        // repeat:-1,
         text:{
             value:headingText,
         }, 
@@ -40,14 +39,7 @@ window.addEventListener('load',() =>{
 })
 
 window.addEventListener('load',() =>{
-    // gsap.from(".pointer", {
-    //     scrollTrigger:{
-    //         trigger: ".pointer",
-    //     },
-    //     duration: 3,
-    //     ease: "power4.out",
-    //     y:50
-    // })
+    //membership-benefits-section
     const textAnimationTl = gsap.timeline({
         scrollTrigger: {
           trigger: ".membership-benefits-section",
@@ -76,4 +68,46 @@ window.addEventListener('load',() =>{
         stagger:0.3
     })
 
+})
+
+window.addEventListener('load',() =>{
+    // blog section
+    const textAnimationT2 = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".blog-section",
+          start: "center center",
+          toggleActions: "restart none restart none",
+        }
+    })
+    textAnimationT2.from(".blog-text", {
+        autoAlpha:0,
+        x:-50,
+        duration:1,
+        ease: "power4.out",
+        stagger:0.5
+    })
+    textAnimationT2.from(".blog-img-container", {
+        autoAlpha:0,
+        y:10,
+        duration: 1,
+        ease: "power4.out",
+        stagger:0.6,
+    },"<0.5")
+})
+
+window.addEventListener('load',() =>{
+    // available-locations-section
+    const textAnimationT3 = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".available-locations-section",
+          start: "center center",
+        }
+    })
+    textAnimationT3.from(".available-locations-text", {
+        autoAlpha:0,
+        y:50,
+        duration:2,
+        ease: "power4.out",
+        stagger:0.7
+    })
 })
